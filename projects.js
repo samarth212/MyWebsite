d624858d2404b16551cc5760344062197e9e4702
Samarth <samarth212@gmail.com>
2024-01-06 12:21:43 -0500

    added ui update with onclick of img on one

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 //let info = 'StudySched is an iOS app that helps students stay organized. Used Swift and XCode to create the application. You can check it out here:'
-let infoHTML = `<p>${info}</p>`
 
 function addInfo(info){
+    let infoHTML = `<p>${info}</p>`
     document.querySelector(".project-info").innerHTML = infoHTML
+   //console.log(document.querySelector(".project-info"))
 }
