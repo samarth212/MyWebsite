ecc92768074b00844b23414a69c10a68636a51fa
Samarth <samarth212@gmail.com>
2024-01-06 12:58:35 -0500

    continued working on project button ui

--- projects.js
+++ projects.js
@@ -1,7 +1,6 @@
 //let info = 'StudySched is an iOS app that helps students stay organized. Used Swift and XCode to create the application. You can check it out here:'
 
 function addInfo(info){
-    let infoHTML = `<p>${info}</p>`
+    let infoHTML = `<p class="studysched">${info}</p>`
     document.querySelector(".project-info").innerHTML = infoHTML
-   //console.log(document.querySelector(".project-info"))
 }
