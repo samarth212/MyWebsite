049b22aa433d6e70d570dc0edcf168cb7e78ddad
Samarth <samarth212@gmail.com>
2024-01-06 13:13:28 -0500

    cont working on proj page

--- projects.js
+++ projects.js
@@ -1,6 +1,13 @@
 //let info = 'StudySched is an iOS app that helps students stay organized. Used Swift and XCode to create the application. You can check it out here:'
 
-function addInfo(info){
-    let infoHTML = `<p class="studysched">${info}</p>`
-    document.querySelector(".project-info").innerHTML = infoHTML
+function addInfo(info, title){
+    let infoHTML = `
+
+    <div class="info-text-div">
+        <p class="proj-info-header">${title}</p>
+        <p class="studysched">${info}</p>
+    </div>
+  
+    `
+    document.querySelector(".info-text").innerHTML = infoHTML
 }
