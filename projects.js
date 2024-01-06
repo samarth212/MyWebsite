df996aa5f8d9bd0fabf21a285a888974cade4be3
Samarth <samarth212@gmail.com>
2024-01-06 13:45:47 -0500

    finished full button functionality

--- projects.js
+++ projects.js
@@ -1,8 +1,11 @@
 //let info = 'StudySched is an iOS app that helps students stay organized. Used Swift and XCode to create the application. You can check it out here:'
 
-function addInfo(info, title){
+function addInfo(info, title, img){
     let infoHTML = `
 
+    <div class="img-container">
+        <img class="proj-img" src="images/Screenshot 2023-11-17 at 7.14.18 PM.png" alt="">
+    </div>
     <div class="info-text-div">
         <p class="proj-info-header">${title}</p>
         <p class="studysched">${info}</p>
